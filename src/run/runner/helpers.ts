00812ef04c1a234f8e1efbc0d9454aa9d76f61d6
Khachik Astoyan <kastoyan@griddynamics.com>
2023-12-10 14:48:39 +0400

    add python support

--- src/run/runner/helpers.ts
+++ src/run/runner/helpers.ts
@@ -1,10 +1,19 @@
-export const supportedLanguages = ["javascript", "rust", "typescript", "go", "c", "cpp"] as const;
+export const supportedLanguages = [
+	"javascript",
+	"rust",
+	"typescript",
+	"go",
+	"c",
+	"cpp",
+	"python",
+] as const;
 export type Language = (typeof supportedLanguages)[number];
 
 const langToExtention: { [key in Language]?: string } = {
 	javascript: "js",
 	rust: "rs",
 	typescript: "ts",
+	python: "py",
 } as const;
 
 export const getExtention = (lang: Language) => langToExtention[lang] ?? lang;
